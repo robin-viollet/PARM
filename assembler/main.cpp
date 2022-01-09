f4aecc669c90abfe5549d783022b3b17198b9b2c
Robin Viollet <robin.viollet@etu.univ-cotedazur.fr>
2022-01-09 14:27:54 +0100

    Improved (again) instruction support
    code_arm is now fully compatible
    
    code_c need improvement

--- assembler/main.cpp
+++ assembler/main.cpp
@@ -15,6 +15,10 @@ using std::chrono::duration_cast;
 using std::chrono::seconds;
 using std::chrono::high_resolution_clock;
 
+std::map<std::string, int> labels;
+int pc = 0;
+bool debug = false;
+
 void replace_all(std::string& str, std::string_view what, std::string_view with){
     std::string::size_type pos{};
     while (std::string::npos != (pos = str.find(what.data(), pos, what.length()))){
@@ -24,26 +28,24 @@ void replace_all(std::string& str, std::string_view what, std::string_view with)
 }
 
 std::bitset<16> parseRegister(std::string& s){
-    if (s[0] == 'r'){
+    if (s.size() > 0 && s[0] == 'r'){
         return std::bitset<3>(std::stoi(s.substr(1))).to_ullong();
     }
-
-    // TODO Fix
-    //return std::bitset<3>(0).to_ullong();
 
     throw exception("register not parsed");
 }
 
 template <int T>
 std::bitset<16> parseImm(std::string& s, bool sp = false){
-    if (s[0] == '#'){
+    if (s.size() > 0 && s[0] == '#'){
         return std::bitset<T>(std::stoi(s.substr(1)) / (sp ? 4 : 1)).to_ullong();
     }
 
     throw exception("immediate not parsed");
 }
 
-std::bitset<16> parseCondition(const std::string& s){
+std::bitset<16> parseCondition(std::string s){
+    transform(s.begin(), s.end(), s.begin(), ::toupper);
     if (s == "EQ")
         return 0b000000000000'0000;
     else if (s == "NE")
@@ -81,6 +83,11 @@ std::bitset<16> parseCondition(const std::string& s){
 template <int T, int U, int V = std::max(T, U)>
 std::bitset<V> operator|(std::bitset<T>& b1, std::bitset<U> b2){
     return std::bitset<V>(b1) |= b2;
+}
+
+template <int T>
+std::bitset<16> parseLabel(const std::string& s, int source){
+    return std::bitset<T>(labels[s] - source - 3).to_ullong();
 }
 
 std::bitset<16> convert_instruction(const std::string& instruction, std::vector<std::string>& args){
@@ -125,7 +132,6 @@ std::bitset<16> convert_instruction(const std::string& instruction, std::vector<
                     parseImm<5>(args[2]) << 6 | parseRegister(args[1]) << 3 | parseRegister(args[0]);
         }
     } else if (std::regex_match(instruction, std::regex("adds?"))){
-        // TODO Complex if (done)
         if (args[0] != "sp" && args.size() == 3){
             if (args.size() == 3 && args[2][0] == 'r'){
                 // Registers
@@ -150,7 +156,6 @@ std::bitset<16> convert_instruction(const std::string& instruction, std::vector<
                        parseRegister(args[0]) << 8 | parseImm<8>(args[args.size() - 1]);
             }
         } else {
-            // TODO Check sp (done)
             // SP + Immediate (7bits)
             // ADD [ SP , ] SP,# <offset>
             // 15 14 13 12 11 10 9 8 7 | 6 5 4 3 2 1 0
@@ -159,21 +164,22 @@ std::bitset<16> convert_instruction(const std::string& instruction, std::vector<
                    parseImm<7>(args[args.size() - 1], true);
         }
     } else if (std::regex_match(instruction, std::regex("subs?"))){
-        // TODO Complex if
         if (args.size() == 3){
-            // Registers
-            // SUBS <Rd > , < Rn > , <Rm>
-            // 15 14 13 12 11 10 9 | 8 7 6 | 5 4 3 | 2 1 0
-            //  0  0  0  1  1  0 1 |    Rm |    Rn |    Rd
-            return std::bitset<16>(0b0001101'000'000'000) |
-                   parseRegister(args[0]) << 6 | parseRegister(args[1]) << 3 | parseRegister(args[0]);
-
-            // Immediate (3bits)
-            // SUBS <Rd > , < Rn> ,# <imm3>
-            // 15 14 13 12 11 10 9 | 8 7 6 | 5 4 3 | 2 1 0
-            //  0  0  0  1  1  1 1 |  Imm3 |    Rn |    Rd
-            return std::bitset<16>(0b0001111'000'000'000) |
-                   parseImm<3>(args[2]) << 6 | parseRegister(args[1]) << 3 | parseRegister(args[0]);
+            if (args[2][0] == '#'){
+                // Immediate (3bits)
+                // SUBS <Rd > , < Rn> ,# <imm3>
+                // 15 14 13 12 11 10 9 | 8 7 6 | 5 4 3 | 2 1 0
+                //  0  0  0  1  1  1 1 |  Imm3 |    Rn |    Rd
+                return std::bitset<16>(0b0001111'000'000'000) |
+                       parseImm<3>(args[2]) << 6 | parseRegister(args[1]) << 3 | parseRegister(args[0]);
+            } else {
+                // Registers
+                // SUBS <Rd > , < Rn > , <Rm>
+                // 15 14 13 12 11 10 9 | 8 7 6 | 5 4 3 | 2 1 0
+                //  0  0  0  1  1  0 1 |    Rm |    Rn |    Rd
+                return std::bitset<16>(0b0001101'000'000'000) |
+                       parseRegister(args[2]) << 6 | parseRegister(args[1]) << 3 | parseRegister(args[0]);
+            }
         } else if (args.size() == 2){
             if (args[0] != "sp"){
                 // Immediate (8bits)
@@ -183,7 +189,6 @@ std::bitset<16> convert_instruction(const std::string& instruction, std::vector<
                 return std::bitset<16>(0b00111'000'00000000) |
                        parseRegister(args[0]) << 8 | parseImm<8>(args[args.size() - 1]);
             } else {
-                // TODO Check sp (done)
                 // Immediate (7bits)
                 // SUB [ SP , ] SP,# <offset>
                 // 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 0
@@ -200,20 +205,21 @@ std::bitset<16> convert_instruction(const std::string& instruction, std::vector<
                    parseRegister(args[0]) << 8 | parseImm<8>(args[1]);
         }
     } else if (std::regex_match(instruction, std::regex("cmp"))){
-        // TODO Complex if
-        // Immediate (8bits)
-        // CMP <Rd> ,# <imm8>
-        // 15 14 13 12 11 | 10 9 8 | 7 6 5 4 3 2 1 0
-        //  0  0  1  0  1 |     Rd |            imm8
-        return std::bitset<16>(0b00101'000'00000000) |
-                parseRegister(args[0]) << 8 | parseImm<8>(args[1]);
-
-        // Registers
-        // CMP <Rn > , <Rm>
-        // 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 0
-        //  0  1  0  0  0  0 1 0 1 0    Rm    Rn
-        return std::bitset<16>(0b0100001010'000'000) |
-                parseRegister(args[1]) << 3 | parseRegister(args[0]);
+        if (args[1][0] == '#'){
+            // Immediate (8bits)
+            // CMP <Rd> ,# <imm8>
+            // 15 14 13 12 11 | 10 9 8 | 7 6 5 4 3 2 1 0
+            //  0  0  1  0  1 |     Rd |            imm8
+            return std::bitset<16>(0b00101'000'00000000) |
+                   parseRegister(args[0]) << 8 | parseImm<8>(args[1]);
+        } else {
+            // Registers
+            // CMP <Rn > , <Rm>
+            // 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 0
+            //  0  1  0  0  0  0 1 0 1 0    Rm    Rn
+            return std::bitset<16>(0b0100001010'000'000) |
+                   parseRegister(args[1]) << 3 | parseRegister(args[0]);
+        }
     } else if (std::regex_match(instruction, std::regex("ands?"))){
         if (args.size() == 2){
             // ANDS <Rdn > , <Rm>
@@ -319,47 +325,51 @@ std::bitset<16> convert_instruction(const std::string& instruction, std::vector<
                     parseRegister(args[1]) << 3 | parseRegister(args[0]);
         }
     } else if (std::regex_match(instruction, std::regex("str"))){
-        // TODO Complex if
         // STR <Rt > , [ SP,# <offset> ]
         // 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 0
         //  1  0  0  1  0     Rt            imm8
         std::bitset<16> bin(0b10010'000'00000000);
 
-        /*if (args.size() == ){
-            bin |= parseImm<>();
-        }*/
-
-        return bin | parseRegister(args[0]) << 8;
+        if (args.size() >= 3){
+            return bin | parseRegister(args[0]) << 8 |
+                   std::bitset<16>(std::bitset<8>(parseImm<16>(args[2]).to_ullong() / 4).to_ullong());
+        } else {
+            return bin | parseRegister(args[0]) << 8;
+        }
     } else if (std::regex_match(instruction, std::regex("ldr"))){
-        // TODO Complex if
         // LDR <Rt > , [ SP{ , # <offset> } ]
         // 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 0
         //  1  0  0  1  1     Rt            imm8
-        return std::bitset<16>(0b10011'000'00000000) |
-                parseRegister(args[0]) << 8;
+        if (args.size() >= 3){
+            return std::bitset<16>(0b10011'000'00000000) |
+                   parseRegister(args[0]) << 8 |
+                   std::bitset<16>(std::bitset<8>(parseImm<16>(args[2]).to_ullong() / 4).to_ullong());
+        } else {
+            return std::bitset<16>(0b10011'000'00000000) |
+                   parseRegister(args[0]) << 8;
+        }
     } else if (std::regex_match(instruction, std::regex("cmp?"))){
-        // TODO Complex if
         // ADD [ SP , ] SP,# <offset>
         // 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 0
         //  1  0  1  1  0  0 0 0 0          imm7
         return std::bitset<16>(0b101100000'0000000) |
                 parseImm<7>(args[args.size() - 1], true);
-    } else if (std::regex_match(instruction, std::regex("b.?"))){
+    } else if (std::regex_match(instruction, std::regex("b.{0,2}"))){
         if (instruction == "b" || instruction == "bx"){
-            // TODO label
             // Immediate (11bits)
             // B <label>
             // 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 0
             //  1  1  1  0  0                  imm11
-            return std::bitset<16>(0b11100'00000000000);
+            return std::bitset<16>(0b11100'00000000000) |
+                    parseLabel<11>(args[0], pc);
         } else {
-            // TODO label
             // Immediate (8bits)
             // B<c> <label>
             // 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 0
             //  1  1  0  1      cond            imm8
             return std::bitset<16>(0b1101'0000'00000000) |
-                    parseCondition(instruction.substr(1)) << 8;
+                    parseCondition(instruction.substr(1)) << 8 |
+                    parseLabel<8>(args[0], pc);
         }
     }
 
@@ -379,7 +389,7 @@ int main(int argc, char** argv){
     auto startTime = high_resolution_clock::now();
     regex extensionRegex("^(.*)[.]s$");
     regex labelRegex("^.*:$");
-    regex instructionRegex("^\t[^.].*$");
+    regex instructionRegex("^\t?[^.@].*$");
     string reason;
 
     try {
@@ -399,25 +409,43 @@ int main(int argc, char** argv){
 
                     std::cout << "assembling in file " << outFile.string() << std::endl;
 
-                    std::string line;
-                    std::map<std::string, int> labels;
-                    int pc = 0;
+                    std::vector<std::string> lines;
 
-                    //out << "2.0 raw" << std::endl;
+                    if (!debug){
 
-                    while (std::getline(in, line)){
+                        out << "v2.0 raw" << std::endl;
+
+                    }
+
+                    {
+                        std::string line;
+
+                        while (std::getline(in, line)){
+                            lines.push_back(line);
+
+                            if (regex_match(line, labelRegex)){
+
+                                std::string label = line.substr(0, line.find(':'));
+                                labels[label] = pc;
+                                std::cout << "label: " << label << "(" << labels[label] << " pc: " << pc << ")" << std::endl;
+
+                            } else if (regex_match(line, instructionRegex)){
+
+                                ++pc;
+
+                            }
+                        }
+                    }
+
+                    pc = 0;
+
+                    for (std::string& line : lines){
                         replace_all(line, ", ", ",");
                         replace_all(line, "[", "");
                         replace_all(line, "]", "");
                         std::istringstream iss(line);
 
-                        if (regex_match(line, labelRegex)){
-
-                            std::string label = line.substr(0, line.find(':'));
-                            labels[label] = pc;
-                            std::cout << "label: " << label << "(" << labels[label] << ")" << std::endl;
-
-                        } else if (regex_match(line, instructionRegex)){
+                        if (regex_match(line, instructionRegex)){
 
                             std::vector<std::string> words{
                                     std::istream_iterator<std::string>(iss),
@@ -434,7 +462,23 @@ int main(int argc, char** argv){
                                 args.push_back(arg);
                             }
 
-                            std::cout << std::hex << convert_instruction(instruction, args).to_ullong() << std::endl;
+                            try {
+
+                                if (debug){
+
+                                    std::cout << std::hex << convert_instruction(instruction, args).to_ullong()
+                                              << std::endl;
+
+                                } else {
+
+                                    out << std::setfill('0') << std::hex << std::setw(4)
+                                        << convert_instruction(instruction, args).to_ullong() << ' ';
+
+                                }
+
+                            } catch (exception& e){
+                                std::cerr << e.what() << std::endl;
+                            }
 
                             std::cout << instruction << '(';
 
@@ -446,6 +490,12 @@ int main(int argc, char** argv){
                             ++pc;
 
                         }
+                    }
+
+                    if (!debug){
+
+                        out << std::endl;
+
                     }
 
                     std::cout << "avengers assembled in " << outFile << " in " <<
